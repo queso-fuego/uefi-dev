6602f47ad24c649b6400015925c4b38601c6a314
Queso Fuego <fuegoqueso@gmail.com>
2023-10-03 22:47:03 -0500

    efi.c: Add main menu & keybinds
    Now prints an initial main menu that allows user to select with Arrow
    keys and Enter. Escape will also shutdown from main menu.
    
    NOTE: Setting a text mode should work the same, although entering an invalid mode,
    and then pressing escape twice seems to break things in emulation. On hardware,
    I haven't come across an invalid text mode yet, but it's something to debug
    and fix when able.

--- efi_c/efi.c
+++ efi_c/efi.c
@@ -1,16 +1,33 @@
 #include <stdarg.h>
 #include "efi.h"
 
+// TODO: Make "eprintf" or "fprintf" or similar function to print to StdErr
+//   instead of ConOut
+
+// -----------------
+// Global macros
+// -----------------
+#define ARRAY_SIZE(x) (sizeof (x) / sizeof (x)[0])
+
 // -----------------
 // Global constants
 // -----------------
-#define scancode_ESC 0x17
+#define SCANCODE_UP_ARROW   0x1
+#define SCANCODE_DOWN_ARROW 0x2
+#define SCANCODE_ESC        0x17
+
+#define DEFAULT_FG_COLOR EFI_YELLOW
+#define DEFAULT_BG_COLOR EFI_BLUE
+
+#define HIGHLIGHT_FG_COLOR EFI_BLUE
+#define HIGHLIGHT_BG_COLOR EFI_CYAN
 
 // -----------------
 // Global variables
 // -----------------
 EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *cout = NULL;  // Console output
-EFI_SIMPLE_TEXT_INPUT_PROTOCOL  *cin = NULL;   // Console input
+EFI_SIMPLE_TEXT_INPUT_PROTOCOL  *cin  = NULL;  // Console input
+EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *cerr = NULL;  // Console output - stderr
 EFI_BOOT_SERVICES    *bs;   // Boot services
 EFI_RUNTIME_SERVICES *rs;   // Runtime services
 EFI_HANDLE image = NULL;    // Image handle
@@ -179,27 +196,15 @@ EFI_INPUT_KEY get_key(void) {
 }
 
 // ====================
-// Entry Point
+// Set Text Mode
 // ====================
-EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-    // Initialize global variables
-    init_global_variables(ImageHandle, SystemTable);
-
-    // Reset Console Output
-    cout->Reset(SystemTable->ConOut, false);
-
-    // Set text colors - foreground, background
-    cout->SetAttribute(SystemTable->ConOut, EFI_TEXT_ATTR(EFI_YELLOW, EFI_BLUE));
-
-    // Screen loop
-    bool running = true;
-    while (running) {
-        // Clear console output; clear screen to background color and
-        //   set cursor to 0,0
-        cout->ClearScreen(SystemTable->ConOut);
+EFI_STATUS set_text_mode(void) {
+    // Overall screen loop
+    while (true) {
+        cout->ClearScreen(cout);
 
         // Write String
-        cout->OutputString(SystemTable->ConOut, u"Text mode information:\r\n");
+        cout->OutputString(cout, u"Text mode information:\r\n");
         UINTN max_cols = 0, max_rows = 0;
 
         // Get current text mode's column and row counts
@@ -222,7 +227,7 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
                max_cols,
                max_rows);
 
-        cout->OutputString(SystemTable->ConOut, u"Available text modes:\r\n");
+        cout->OutputString(cout, u"Available text modes:\r\n");
 
         // Print other text mode infos
         const INT32 max = cout->Mode->MaxMode;
@@ -232,7 +237,7 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
         }
 
         // Get number from user
-        while (1) {
+        while (true) {
             static UINTN current_mode = 0;
             current_mode = cout->Mode->Mode;
 
@@ -253,11 +258,9 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
             // Process keystroke
             printf(u"%s ", cbuf);
 
-            if (key.ScanCode == scancode_ESC) {
-                // ESC Key, Quit & Shutdown
-                rs->ResetSystem(EfiResetShutdown, EFI_SUCCESS, 0, NULL);
-
-                // !NOTE!: This should not return, system should power off
+            if (key.ScanCode == SCANCODE_ESC) {
+                // Go back to main menu
+                return EFI_SUCCESS;
             }
 
             // Choose text mode & redraw screen
@@ -266,16 +269,150 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
             if (EFI_ERROR(status)) {
                 // Handle error
                 if (status == EFI_DEVICE_ERROR) { 
-                    printf(u"ERROR: %x; Device Error\r", status);
+                    printf(u"ERROR: %x; Device Error", status);
                 } else if (status == EFI_UNSUPPORTED) {
-                    printf(u"ERROR: %x; Mode # is invalid\r", status);
+                    printf(u"ERROR: %x; Mode # is invalid", status);
                 }
+                printf(u"\r\nPress any key to select again", status);
                 get_key();
-            } else {
-                break; // Successfully set new text mode, redraw screen
+
+            } 
+
+            // Set new mode, redraw screen from outer loop
+            break;
+        }
+    }
+
+    return EFI_SUCCESS;
+}
+
+// ====================
+// Set Graphics Mode
+// ====================
+EFI_STATUS set_graphics_mode(void) {
+    return EFI_SUCCESS;
+}
+
+// ====================
+// Entry Point
+// ====================
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+    // Initialize global variables
+    init_global_variables(ImageHandle, SystemTable);
+
+    // Reset Console Output
+    cout->Reset(cout, false);
+
+    // Set text colors - foreground, background
+    cout->SetAttribute(cout, EFI_TEXT_ATTR(DEFAULT_FG_COLOR, DEFAULT_BG_COLOR));
+
+    // Screen loop
+    bool running = true;
+    while (running) {
+        const CHAR16 *menu_choices[] = {
+            u"Set Text Mode",
+            u"Set Graphics Mode",
+        };
+
+        EFI_STATUS (*menu_funcs[])(void) = {
+            set_text_mode,
+            set_graphics_mode,
+        };
+
+        // Clear console output; clear screen to background color and
+        //   set cursor to 0,0
+        cout->ClearScreen(cout);
+
+        // Get current text mode ColsxRows values
+        UINTN cols = 0, rows = 0;
+        cout->QueryMode(cout, cout->Mode->Mode, &cols, &rows);
+
+        // Print keybinds at bottom of screen
+        cout->SetCursorPosition(cout, 0, rows-3);
+        printf(u"Up/Down Arrow = Move cursor\r\n"
+               u"Enter = Select\r\n"
+               u"Escape = Shutdown");
+
+        // Print menu choices
+        // Highlight first choice as initial choice
+        cout->SetCursorPosition(cout, 0, 0);
+        cout->SetAttribute(cout, EFI_TEXT_ATTR(HIGHLIGHT_FG_COLOR, HIGHLIGHT_BG_COLOR));
+        printf(u"%s", menu_choices[0]);
+
+        // Print rest of choices
+        cout->SetAttribute(cout, EFI_TEXT_ATTR(DEFAULT_FG_COLOR, DEFAULT_BG_COLOR));
+        for (UINTN i = 1; i < ARRAY_SIZE(menu_choices); i++)
+            printf(u"\r\n%s", menu_choices[i]);
+
+        // Get cursor row boundaries
+        INTN min_row = 0, max_row = cout->Mode->CursorRow;
+
+        // Input loop
+        cout->SetCursorPosition(cout, 0, 0);
+        bool getting_input = true;
+        while (getting_input) {
+            INTN current_row = cout->Mode->CursorRow;
+            EFI_INPUT_KEY key = get_key();
+
+            // Process input
+            switch (key.ScanCode) {
+                case SCANCODE_UP_ARROW:
+                    if (current_row-1 >= min_row) {
+                        // De-highlight current row, move up 1 row, highlight new row
+                        cout->SetAttribute(cout, EFI_TEXT_ATTR(DEFAULT_FG_COLOR, DEFAULT_BG_COLOR));
+                        printf(u"%s\r", menu_choices[current_row]);
+
+                        current_row--;
+                        cout->SetCursorPosition(cout, 0, current_row);
+                        cout->SetAttribute(cout, EFI_TEXT_ATTR(HIGHLIGHT_FG_COLOR, HIGHLIGHT_BG_COLOR));
+                        printf(u"%s\r", menu_choices[current_row]);
+
+                        // Reset colors
+                        cout->SetAttribute(cout, EFI_TEXT_ATTR(DEFAULT_FG_COLOR, DEFAULT_BG_COLOR));
+                    }
+                    break;
+
+                case SCANCODE_DOWN_ARROW:
+                    if (current_row+1 <= max_row) {
+                        // De-highlight current row, move down 1 row, highlight new row
+                        cout->SetAttribute(cout, EFI_TEXT_ATTR(DEFAULT_FG_COLOR, DEFAULT_BG_COLOR));
+                        printf(u"%s\r", menu_choices[current_row]);
+
+                        current_row++;
+                        cout->SetCursorPosition(cout, 0, current_row);
+                        cout->SetAttribute(cout, EFI_TEXT_ATTR(HIGHLIGHT_FG_COLOR, HIGHLIGHT_BG_COLOR));
+                        printf(u"%s\r", menu_choices[current_row]);
+
+                        // Reset colors
+                        cout->SetAttribute(cout, EFI_TEXT_ATTR(DEFAULT_FG_COLOR, DEFAULT_BG_COLOR));
+                    }
+                    break;
+
+                case SCANCODE_ESC:
+                    // Escape key: power off
+                    rs->ResetSystem(EfiResetShutdown, EFI_SUCCESS, 0, NULL);
+
+                    // !NOTE!: This should not return, system should power off
+                    break;
+
+                default:
+                    if (key.UnicodeChar == u'\r') {
+                        // Enter key, select choice
+                        EFI_STATUS return_status = menu_funcs[current_row]();
+                        if (EFI_ERROR(return_status)) {
+                            // TODO: Change to write to stderr with cerr, not cout
+                            printf(u"ERROR %x\r\n; Press any key to go back...", return_status);
+                            get_key();
+                        }
+
+                        // Will leave input loop and reprint main menu
+                        getting_input = false; 
+                    }
+                    break;
             }
         }
     }
 
+    // End program
     return EFI_SUCCESS;
 }
