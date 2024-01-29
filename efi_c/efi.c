53054ca1c0d2f3c172d377f1bccb6638dc328632
Queso Fuego <fuegoqueso@gmail.com>
2024-01-28 20:43:52 -0600

    Fix memcmp
    Program after coffee next time. memcmp() should return difference
    in dst & src byte when not equal, not difference in dst byte offsets.
    I had typed what I was saying at the time, not thinking.

--- efi_c/efi.c
+++ efi_c/efi.c
@@ -104,7 +104,7 @@ INTN memcmp(VOID *m1, VOID *m2, UINTN len) {
     UINT8 *p = m1;
     UINT8 *q = m2;
     for (UINTN i = 0; i < len; i++)
-        if (p[i] != q[i]) return (INTN)p[1] - (INTN)p[2];
+        if (p[i] != q[i]) return (INTN)(p[i]) - (INTN)(q[i]);
 
     return 0;
 }
