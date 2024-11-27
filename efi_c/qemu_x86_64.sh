a3749f29026e9ae7901d6dc429df6e2189672d8f
fuegoqueso@gmail.com <fuegoqueso@gmail.com>
2024-11-26 19:08:46 -0600

    Set default x86 bios path to submodule dir.

--- efi_c/qemu_x86_64.sh
+++ efi_c/qemu_x86_64.sh
@@ -3,7 +3,7 @@
 # Sendin' Out a TEST O S
 qemu-system-x86_64 \
 -drive format=raw,file=../UEFI-GPT-image-creator/test.hdd \
--bios bios64.bin \
+-bios ../UEFI-GPT-image-creator/bios64.bin \
 -m 256M \
 -vga std \
 -display gtk,gl=on,zoom-to-fit=off,window-close=on \
