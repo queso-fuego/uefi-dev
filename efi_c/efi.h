d68a57938827e22d8bc074e846e3e399550bb006
Jason Garner <elevatorguy@gmail.com>
2025-01-03 12:50:11 -0600

    EFI_SIMPLE_NETWORK_MODE
    per page 926 of UEFI Spec - 2.11:
    "All these fields must be discovered in a protocol instance of EFI_DRIVER_BINDING_PROTOCOL.Start()"

--- efi_c/efi.h
+++ efi_c/efi.h
@@ -201,6 +201,28 @@ const CHAR16 *EFI_ERROR_STRINGS[MAX_EFI_ERROR] = {
 };
 
 // EFI_SIMPLE_NETWORK_PROTOCOL
+typedef struct {
+  UINT32                                    State;
+  UINT32                                    HwAddressSize;
+  UINT32                                    MediaHeaderSize;
+  UINT32                                    MaxPacketSize;
+  UINT32                                    NvRamSize;
+  UINT32                                    NvRamAccessSize;
+  UINT32                                    ReceiveFilterMask;
+  UINT32                                    ReceiveFilterSetting;
+  UINT32                                    MaxMCastFilterCount;
+  UINT32                                    MCastFilterCount;
+  EFI_MAC_ADDRESS                           MCastFilter[MAX_MCAST_FILTER_CNT];
+  EFI_MAC_ADDRESS                           CurrentAddress;
+  EFI_MAC_ADDRESS                           BroadcastAddress;
+  EFI_MAC_ADDRESS                           PermanentAddress;
+  UINT8                                     IfType;
+  BOOLEAN                                   MacAddressChangeable;
+  BOOLEAN                                   MultipleTxSupported;
+  BOOLEAN                                   MediaPresentSupported;
+  BOOLEAN                                   MediaPresent;
+} EFI_SIMPLE_NETWORK_MODE;
+
 typedef struct EFI_SIMPLE_NETWORK_PROTOCOL {
   UINT64                                    Revision;
   EFI_SIMPLE_NETWORK_START                  Start;
